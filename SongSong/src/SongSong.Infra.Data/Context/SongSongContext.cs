a60e4c8fd91dc68b076b3a4df86dda9a04f3d07b
METAORA\paulo.campez <prcampez@gmail.com>
2019-11-20 15:46:06 -0300

    Adicionado context e config

---
+++ SongSong/src/SongSong.Infra.Data/Context/SongSongContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SongSong.Infra.Data.Context
+{
+    public class SongSongContext : DbContext
+    {
+        private readonly IHostingEnvironment _env;
+
+        public SongSongContext(IHostingEnvironment env)
+        {
+            _env = env;
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(_env.ContentRootPath)
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            // escolher banco
+            //optionsBuilder.UseSqlServer(config.GetConnectionString("DefaultConnection"));
+        }
+    }
+}
