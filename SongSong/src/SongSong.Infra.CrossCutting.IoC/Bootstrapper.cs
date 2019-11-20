c6563505de1c33e13c05cafea501c676564a633a
METAORA\paulo.campez <prcampez@gmail.com>
2019-11-20 17:49:26 -0300

    Registrado dependencia UoW e context

--- SongSong/src/SongSong.Infra.CrossCutting.IoC/Bootstrapper.cs
+++ SongSong/src/SongSong.Infra.CrossCutting.IoC/Bootstrapper.cs
@@ -1,4 +1,9 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using SongSong.Application.Interfaces;
+using SongSong.Application.Services;
+using SongSong.Domain.Interfaces;
+using SongSong.Infra.Data.Context;
+using SongSong.Infra.Data.UnitOfWork;
 using System;
 
 namespace SongSong.Infra.CrossCutting.IoC
@@ -7,7 +12,12 @@ namespace SongSong.Infra.CrossCutting.IoC
     {
         public static void RegisterServices(IServiceCollection service)
         {
+            //Data
+            service.AddScoped<IUnitOfWork, UnitOfWork>();
+            service.AddScoped<SongSongContext>();
 
+            //Application
+            service.AddScoped<IAlbumAplicationService, AlbumApplicationService>();
         }
     }
 }
