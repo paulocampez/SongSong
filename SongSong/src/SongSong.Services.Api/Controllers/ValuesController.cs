06ec0d44e41676350111c6125e1088ff5e2deea6
METAORA\paulo.campez <prcampez@gmail.com>
2019-11-20 14:26:40 -0300

    Primeiro teste unitario

--- SongSong/src/SongSong.Services.Api/Controllers/ValuesController.cs
+++ SongSong/src/SongSong.Services.Api/Controllers/ValuesController.cs
@@ -14,14 +14,14 @@ namespace SongSong.Services.Api.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "value1", "value2" };
+            return Ok(new string[] { "value1", "value2" }); 
         }
 
         // GET api/values/5
         [HttpGet("{id}")]
         public ActionResult<string> Get(int id)
         {
-            return "value";
+            return Ok("value");
         }
 
         // POST api/values
