988214a5ff11e6cbb84e7846a64015452db28bff
METAORA\paulo.campez <prcampez@gmail.com>
2019-11-20 17:44:29 -0300

    Adicionado mediatR + projeto ioc

---
+++ SongSong/src/SongSong.Infra.CrossCutting.IoC/Bootstrapper.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace SongSong.Infra.CrossCutting.IoC
+{
+    public class Bootstrapper
+    {
+        public static void RegisterServices(IServiceCollection service)
+        {
+
+        }
+    }
+}
