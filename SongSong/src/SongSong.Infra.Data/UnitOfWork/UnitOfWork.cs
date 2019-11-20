b38bc1612b4ca79a4031de02e7bde0867ca5c05c
METAORA\paulo.campez <prcampez@gmail.com>
2019-11-20 15:37:26 -0300

    Evolucao na estrutura do codigo

---
+++ SongSong/src/SongSong.Infra.Data/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,24 @@
+﻿using SongSong.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SongSong.Infra.Data.UnitOfWork
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+
+        //Construtor
+        public UnitOfWork()
+        { }
+        public bool Commit()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
